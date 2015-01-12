52426fcaa24b9528dc20155f29ef25e39566122d
unknown <pblo28@gmail.com>
2015-01-12 13:40:11 +0100

    XML parser for building forward and return messages structure.

---
+++ MessagesParser/BSMessagesParser.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+xmlStr = open("2VmsMessages.xml").read()
+xmlObj = BeautifulSoup(xmlStr,"xml")
+
+forwardMessages = xmlObj.find("ForwardMessages").findAll("Message")
+returnMessages =  xmlObj.find("ReturnMessages").findAll("Message")
+
+outputForward = []
+outputReturn = []
+
+print "FORWARD  MSGs:"
+
+for msg in forwardMessages:
+  outputForward.append({ "name" : msg.Name.string, "Min" : msg.MIN.string })
+  print "{ name =\"%s\", min=%s}"%(msg.Name.string,msg.MIN.string)
+
+print "****"
+print "RETURN  MSGs:"
+
+for msg in returnMessages:
+  outputReturn.append({ "name" : msg.Name.string, "Min" : msg.MIN.string })
+  print "{ name =\"%s\", min=%s}"%(msg.Name.string,msg.MIN.string)
+
+
