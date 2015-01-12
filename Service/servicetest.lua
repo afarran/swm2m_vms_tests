03b21ffbbc4e2299273ef9028ac36328faf82a11
brabiega <brabiega@pl.sii.eu>
2015-01-12 11:21:31 +0100

    ServiceWrapper - added property encoding

--- Service/servicetest.lua
+++ Service/servicetest.lua
@@ -3,7 +3,9 @@ require "Service/EioServiceWrapper"
 eio = EioServiceWrapper()
 -- a = service:getPropertiesByName({"latitude", "longitude"})
 
-a = eio:getPropertiesByName({"port1Config"}, false)
+--a = eio:getPropertiesByName({"port1Config"}, false)
 --position:setPropertiesByName({latitude = 1, continuous = 1})
 --a = position:getPropertiesByName({"latitude", "continuous"})
+a = eio:setPropertiesByName({port1Config = "Disabled"})
+a = eio:getPropertiesByName({"port1Config"}, false)
 print(eio.pins)
