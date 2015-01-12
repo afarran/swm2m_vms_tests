137db1fe79b7962a53f4e5cb0fdf64ff9e2e9870
unknown <pblo28@gmail.com>
2015-01-12 12:11:42 +0100

    Initialization of TestReportingModule.

---
+++ TestReportingModule.lua
@@ -0,0 +1,31 @@
+-----------
+-- Reporting test module
+-- - contains VMS reporting features
+-- @module TestReportingModule
+
+module("TestReportingModule", package.seeall)
+
+
+function suite_setup()
+  -- reset of properties 
+  -- restarting VMS agent ?
+end
+
+-- executed after each test suite
+function suite_teardown()
+end
+
+--- setup function
+function setup()
+end
+
+-----------------------------------------------------------------------------------------------
+--- teardown function executed after each unit test
+function teardown()
+  
+end
+
+-------------------------
+-- Test Cases
+-------------------------
+
