21933c16f6e1b49e64cc94bd8706ece5f46f295c
brabiega <brabiega@pl.sii.eu>
2015-05-07 11:00:44 +0200

    Moved outputparser to tools. Added TestRunner for better integration with jenkins

--- RunAllModules.lua
+++ RunAllModules.lua
@@ -5,22 +5,21 @@
 -- [-t] [<string pattern>]  Execute test cases that match string pattern
 -- [-s] [<string pattern>]  Execute test suites that match string pattern
 -- [-p] [<port>]  Use specific gateway port
--- [-c] [<FilePath>]  Use Test configuration given in FilePath
+-- [-com] [<comport>]  Use specific com port for console communication
 
 for idx, val in ipairs(arg) do 
   print(idx, val) 
-  if val == "-c" then
-    ConfigFile = arg[idx+1]
-  end
   
   if val == "-p" then
     GatewayPort = arg[idx+1]
+    print("Using port " .. GatewayPort)
+  end
+  
+  if val == "-com" then
+    ComPort = arg[idx+1]
   end
 end
-
-if ConfigFile then
-  print("USING CONFIG: " .. ConfigFile)
-end
+ComPort = ComPort or "com"
 
 -- Annotations
 require("Infrastructure/Annotations/Annotations")
@@ -34,7 +33,7 @@ GPS_PROCESS_TIME = 1                                                -- seconds
 GATEWAY_TIMEOUT = 60                                                -- in seconds
 TIMEOUT_MSG_NOT_EXPECTED = 20                                       -- in seconds
 GEOFENCE_INTERVAL = 10                                              -- in seconds
-GPS_READ_INTERVAL = 1                                               -- used to configure the time interval of updating the position , in seconds
+GPS_READ_INTERVAL = 1          -- used to configure the time interval of updating the position , in seconds
 
 -- Debugger
 require("Debugger/Debugger")
@@ -60,7 +59,7 @@ shellSW = ShellServiceWrapper()
 uniboxSW = UniboxServiceWrapper()
 
 require("Serial/RealSerialWrapper")
-serialMain = RealSerialWrapper({name="com201", open=true, newline="\r\n"})
+serialMain = RealSerialWrapper({name=ComPort, open=true, newline="\r\n"})
 
 -- Helm Panel
 helmPanelFactory = require("HelmPanelDevice/HelmPanelDeviceFactory")()
---
+++ Tools/TestRunner/run.bat
@@ -0,0 +1,2 @@
+python TestRunner.py --modemsim C:/BR/Appsy/IDPToolkit/Applications_v220/ModemSimulator.exe --firmwaredir C:/BR/Projektowe/Firmware --test test_SMTP_WhenHELOCommandCalled_ServerReturns250
+pause
