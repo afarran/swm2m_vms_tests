ba9ba761a595e88f10ec75b065a749b36a68d69c
pblo <pblo28@gmail.com>
2015-05-21 10:48:26 +0200

    Refactored SmtpWrapper. Added Pop3Wrapper.

--- Email/Pop3Wrapper.lua
+++ Email/Pop3Wrapper.lua
@@ -2,9 +2,9 @@ cfg, framework, gateway, lsf, device, gps = require "TestFramework"()
 
 require("Serial/ShellWrapper")
 
-RsShellWrapper = {}
-  RsShellWrapper.__index = RsShellWrapper
-  setmetatable(RsShellWrapper, {
+Pop3Wrapper = {}
+  Pop3Wrapper.__index = Pop3Wrapper
+  setmetatable(Pop3Wrapper, {
     __index = ShellWrapper, -- this is what makes the inheritance work
     __call = function(cls, ...)
       local self = setmetatable({}, cls)
@@ -12,11 +12,10 @@ RsShellWrapper = {}
       return self
     end,})
 
-  function RsShellWrapper:start()
+  function Pop3Wrapper:start()
     local response = self:request("")
-    D:log("RsShellWrapper start.")
-    if string.match(response, ".*mail>") then
-      D:log("Switching to rs shell")
-      self:request("shell")
+    if not string.match(response, ".*mail>") then
+      self:request("mail")
     end
+    self:execute("pop3")
   end
